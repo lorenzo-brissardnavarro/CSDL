7df9b69060a3473437f53fe9ce7564fdc60b4f55
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-09-15 20:28:34 +0200

    feat : loop to keep the window open until the cross is clicked

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,17 @@ int main() {
     // jeu.demarrer();
     Window window("Fenêtre SDL", 800, 600);
     window.windowInitialisation();
+
+    SDL_Event events;
+    bool isOpen = true;
+
+    while (isOpen) {
+        while (SDL_PollEvent(&events)) {
+            switch (events.type) {
+                case SDL_EVENT_QUIT:
+                    isOpen = false;
+                    break;
+            }
+        }
+    }
 }
